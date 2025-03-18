f26223e0c9d781d93e24def323e1431b25e189de
Aguthankgod <Aguthankgod@gmail.com>
2025-03-18 10:23:12 +0100

    refactor app.js to include middleware for token verification and authorization, and improve error handling

--- src/app.js
+++ src/app.js
@@ -5,17 +5,37 @@ const logger = require("./config/logger");
 
 const express = require("express");
 const envJwtKey = require("./utill/envJwtKey");
+const tokenValidityMiddleWare = require("./middlewares/verifyTokenMiddleWare");
+const PermissionMiddleWare = require("./middlewares/authorisation.middleWare");
+const Error_mw = require("./middlewares/errorMiddleware");
 const app = express();
-
-if (!envJwtKey()) {
-  logger.error("FATAL ERROR: jwtsecret is not defined.");
-  throw new Error("FATAL ERROR: jwtsecret is not defined.");
-}
+const cors = require("cors");
 app.use(express.json());
 
-app.use("/api/users", tryCatch_m(require("./routes/auth-routes")));
-
-// app.use(require("./middlewares/errorHandler"));
+try {
+  if (!envJwtKey()) {
+    logger.error("FATAL ERROR: jwtsecret is not defined.");
+    throw new Error("FATAL ERROR: jwtsecret is not defined.");
+  }
+} catch (err) {
+  logger.error(err.message);
+  console.log(err.message);
+}
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use("/api/auth", require("./routes/auth-routes"));
+app.use(
+  "/api/task",
+  tokenValidityMiddleWare,
+  PermissionMiddleWare,
+  require("./routes/task-routes")
+);
+app.use(
+  "/api/user",
+  tokenValidityMiddleWare,
+  PermissionMiddleWare,
+  require("./routes/user-routes")
+);
+app.use(Error_mw);
 
 // Start the server
 module.exports = app;
