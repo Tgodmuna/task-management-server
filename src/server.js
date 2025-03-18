cadc25dc344eaa41d4e0fa4f5094a3cae53958d3
Aguthankgod <Aguthankgod@gmail.com>
2025-03-18 10:21:26 +0100

    remove server.js as part of project restructuring

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const logger = require("../src/config/logger");
-const app = require("./app");
-const envPort = require("./utill/envPORT");
-
-app.listen(envPort(), () => {
-  logger.info(`Server is running on port ${envPort()}`);
-});
